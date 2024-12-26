80b1acf7c0af56bfa4f10ae69bdfea1dd715ab77
Gilbert Tarus <tarusgilbert226@gmail.com>
2024-12-26 13:01:32 +0300

    Removed the generic password for the email for the SMTP

--- oxynet_new/settings.py
+++ oxynet_new/settings.py
@@ -163,7 +163,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'web2024test@gmail.com'
-# EMAIL_HOST_PASSWORD = 'mhlsbgqokbnzkybe'
 EMAIL_HOST_USER = 'insert email'
 EMAIL_HOST_PASSWORD = 'insert password associated with email'
